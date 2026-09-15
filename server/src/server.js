615ff64ef05f7e912f974f11369ed5031e591fed
Vinchef <168990074+Vinchef@users.noreply.github.com>
2026-09-16 02:11:33 +0800

    quick fix 7

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/api/users', usersRoutes);
 app.use('/api/documents', presenceRoutes);

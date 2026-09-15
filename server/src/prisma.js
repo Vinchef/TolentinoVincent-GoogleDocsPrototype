6e2e3fdb09daa9be6fb942dee276ff5963da530b
Vinchef <168990074+Vinchef@users.noreply.github.com>
2026-09-16 00:32:42 +0800

    quick fix 3 vercel

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,4 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import path from 'path';
+
+if (!process.env.DATABASE_URL) {
+  const dbPath = path.resolve(process.cwd(), 'server', 'prisma', 'dev.db');
+  process.env.DATABASE_URL = `file:${dbPath}`;
+}
 
 const prisma = new PrismaClient();
 
